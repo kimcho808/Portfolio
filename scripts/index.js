3f0ae858606f2a9146304bfaf441299181279aa5
kimcho808 <yooa2468@gmail.com>
2026-09-01 15:43:04 +0900

    마샬 완성

--- scripts/index.js
+++ scripts/index.js
@@ -5,3 +5,12 @@ const portfolioSlide = new Swiper('.portfolio',{
         invert:false,
     },
 })
+
+const navLinks = document.querySelectorAll('nav a');
+
+navLinks.forEach((link, index) => {
+    link.addEventListener('click', (e) => {
+        e.preventDefault(); // <a> 태그의 기본 튕김 현상 막기
+        portfolioSlide.slideTo(index); // portfolioSlide 변수를 사용해서 해당 번호로 이동!
+    });
+});s
